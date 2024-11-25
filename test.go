767af47cbf29a26f3ffb1610356996d84ebe211b
anagnostouJohn <anagnostou_j@yahoo.com>
2024-11-25 09:02:37 +0200

    34r34r34r34r

--- test.go
+++ test.go
@@ -3,25 +3,25 @@ package main
 import "fmt"
 
 func main() {
-	prevsize := 0
-	size := 4
-	step := 4
-	x := []string{"a", "jj", "nPe", "NO", "fJ", "o", "C", "DYY", "vY", "kfy", "XWJ", "Y", "vM", "EHP", "B", "Lur", "s", "P", "KsU", "e", "LWc", "Wp", "Vqb", "kz", "b", "FS", "lqM", "ptZ", "bi", "WPf"}
+	// prevsize := 0
+	// size := 4
+	// step := 4
+	// x := []string{"a", "jj", "nPe", "NO", "fJ", "o", "C", "DYY", "vY", "kfy", "XWJ", "Y", "vM", "EHP", "B", "Lur", "s", "P", "KsU", "e", "LWc", "Wp", "Vqb", "kz", "b", "FS", "lqM", "ptZ", "bi", "WPf"}
+	TempTemp := make([][]float64, 5)
+	// for {
 
-	for {
+	// 	if prevsize > len(x) {
+	// 		prevsize = prevsize - step
+	// 		fmt.Println(prevsize)
+	// 		fmt.Println((x[prevsize:]))
+	// 		break
+	// 	}
+	// 	fmt.Println(prevsize, "-----------", size)
+	// 	fmt.Println((x[prevsize:size]))
 
-		if prevsize > len(x) {
-			prevsize = prevsize - step
-			fmt.Println(prevsize)
-			fmt.Println((x[prevsize:]))
-			break
-		}
-		fmt.Println(prevsize, "-----------", size)
-		fmt.Println((x[prevsize:size]))
+	// 	prevsize = prevsize + step
+	// 	size = size + step
 
-		prevsize = prevsize + step
-		size = size + step
-
-	}
-
+	// }
+	fmt.Println(TempTemp)
 }
